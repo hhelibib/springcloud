7dba0f50d3ac7ff63e241417bb557ada82d2ecac
jyh <sclarkca@163.com>
2020-09-20 16:57:59 +0800

    20200920

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jyh&wq
+ * @create 2020-09-19 21:01
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
